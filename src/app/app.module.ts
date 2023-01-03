4fa4502e6e0ec69e8394ef729eed9e1c36b0e737
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2023-01-03 13:59:47 -0600

    ADD: Home view
    It was added the Home view with its dependence componenst, such as,
    products, categories, header and footer components. All of these
    componens were created in this commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './ui/categories/categories.component';
+import { CategoryComponent } from './ui/categories/category/category.component';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { ProductsComponent } from './ui/products/products.component';
+import { ProductComponent } from './ui/products/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CategoriesComponent,
+    CategoryComponent,
+    FooterComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
